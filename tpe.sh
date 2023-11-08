d0145aa9444df2466aabb041aa9946f56d7881cc
conradohillar <hillarconrado@gmail.com>
2023-11-08 10:21:25 -0300

    added .gitignore

--- tpe.sh
+++ tpe.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-curl http://api.sportradar.us/rugby-league/trial/v3/en/seasons.xml?api_key=${API_KEY} -o seasons.xml
+export API_KEY=2f9npdypfkpd8e7thcjnssd5
+
+curl http://api.sportradar.us/rugby-league/trial/v3/en/seasons.xml?api_key=${API_KEY} -o seasons.xml 
+curl http://api.sportradar.us/rugby-league/trial/v3/en/seasons.xml?api_key=${API_KEY} -o season_info.xml
+curl http://api.sportradar.us/rugby-league/trial/v3/en/seasons.xml?api_key=${API_KEY} -o season_lineups.xml

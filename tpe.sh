0457ea2c0543d087160e972dc086bd5132e98d0a
MANUEL AHUMADA <mahumada@lasalleflorida.edu.ar>
2023-11-08 10:31:49 -0300

    Added script argument number validation

--- tpe.sh
+++ tpe.sh
@@ -2,6 +2,11 @@
 
 export API_KEY=2f9npdypfkpd8e7thcjnssd5
 
+if [ $# -ne 2 ]; then
+  echo "Invalid argument number. Arguments must be Prefix Year".
+  exit 1
+fi
+
 curl http://api.sportradar.us/rugby-league/trial/v3/en/seasons.xml?api_key=${API_KEY} -o seasons.xml 
 curl http://api.sportradar.us/rugby-league/trial/v3/en/seasons.xml?api_key=${API_KEY} -o season_info.xml
 curl http://api.sportradar.us/rugby-league/trial/v3/en/seasons.xml?api_key=${API_KEY} -o season_lineups.xml

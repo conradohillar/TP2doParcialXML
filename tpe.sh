c68391b0fc7cfcd3af62d085b03a05ab397f4ace
JuaniGit <jlategana@itba.edu.ar>
2023-11-08 13:35:11 -0300

    Added bandwidth limiter

--- tpe.sh
+++ tpe.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export API_KEY="2f9npdypfkpd8e7thcjnssd5"
+delay=1.1
 
 # Validate argument number
 if [ $# -ne 2 ]; then
@@ -13,6 +14,7 @@ echo "Processing league: $1 $2"
 
 # Download seasons.xml from API
 curl -s http://api.sportradar.us/rugby-league/trial/v3/en/seasons.xml?api_key=${API_KEY} -o seasons.xml 
+sleep(${delay})
 # Delete namespace from seasons.xml
 sed -i'' -e 's@xmlns="http://schemas.sportradar.com/sportsapi/rugby-league/v3"@@g' seasons.xml
 sed -i'' -e 's@xsi:schemaLocation="http://schemas.sportradar.com/sportsapi/rugby-league/v3 https://schemas.sportradar.com/sportsapi/rugby-league/v3/schemas/seasons.xsd"@@g' seasons.xml
@@ -25,11 +27,13 @@ season_id=`java net.sf.saxon.Query -q:extract_season_id.xq season_prefix=$1 seas
 echo "Selected season with id ${season_id}"
 # Download season_info.xml
 curl -s http://api.sportradar.us/rugby-league/trial/v3/en/seasons/${season_id}/info.xml?api_key=${API_KEY} -o season_info.xml
+sleep(${delay})
 
-echo "Downloaded season_info.xml"
+echo "Downloaded season_info.xml"extract_season_data.xq
 
 
 # Download season_lineups.xml
 curl -s http://api.sportradar.us/rugby-league/trial/v3/en/seasons/${season_id}/info.xml?api_key=${API_KEY} -o season_lineups.xml
+sleep(${delay})
 
 echo "Downloaded season_lineups.xml"
